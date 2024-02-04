eb7b4713206007e4f1a290f95eaf63301335801e
Shariar Rafi <shariar.rafi364@gmail.com>
2024-02-05 02:11:33 +0600

    all files uploaded

---
+++ js/srcipt.js
@@ -0,0 +1,45 @@
+$('.main_banner_slider').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    speed: 1000,
+    fade: true,
+});
+
+$('.main_slider').slick({
+    arrows: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow:`<i class="fa-solid fa-angle-left left"></i>`,
+    nextArrow:`<i class="fa-solid fa-angle-right right"></i>`,
+    centerMode: true,
+    centerPadding: '0',
+    responsive: [
+        {
+          breakpoint:  991.98,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 767.98,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 575.98,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerMode: false,
+          }
+        }
+      ]
+    
+});
